a1adbf9b489b896a098fa9d1dc1c1ff77917825c
Dev-scott <sadoscott25@gmail.com>
2025-08-28 13:05:49 +0100

    feat: add menu drawer

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,29 @@
-import React from "react";
+import { useState } from "react";
 import MaxWidthSection from "../MaxWidthSection";
 import Button from "../Button";
+import { Menu } from "lucide-react";
+import { Box, Divider, Drawer } from "@mui/material";
 
 const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const toggleDrawer = (open: boolean) => () => {
+    setOpenMenu(open);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <Box className="px-4 py-2">
+        <Button variant="ghost">Login</Button>
+        <Button variant="primary" className="mt-3">
+          Create account
+        </Button>
+      </Box>
+      <Divider />
+      <Box></Box>
+    </Box>
+  );
+
   return (
     <MaxWidthSection>
       <div className="w-full h-16 flex justify-between items-center ">
@@ -11,7 +32,19 @@ const Navbar = () => {
             CLOUDSHARE
           </span>
         </div>
-        <div className="flex justify-center items-center gap-x-4">
+        <div className="md:hidden block">
+          <Button
+            variant="ghost"
+            onClick={toggleDrawer(true)}
+            className="px-1 py-1"
+          >
+            <Menu />
+          </Button>
+          <Drawer open={openMenu} onClose={toggleDrawer(false)}>
+            {DrawerList}
+          </Drawer>
+        </div>
+        <div className=" hidden md:flex justify-center items-center gap-x-4">
           <Button variant="ghost">Login</Button>
           <Button variant="primary">Create account</Button>
         </div>
